dd06cfb932a69dc259e51e0ac02955e560db32f4
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2024-11-03 14:29:48 +0600

    Initialize Project

---
+++ src/main/java/com/khadok/KhadokOnlineFoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.khadok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhadokOnlineFoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhadokOnlineFoodOrderingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/khadok/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.khadok.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="HG7eVa6I88+FXW7KhKLXOvrrzA59C3aZ1kdd9UUDG4KzjIOzc3ComBjtd9i5ZYM/ARusWm+M2K3Cgz4+gKpttg==";
+    public  static final String JWT_HEADER="Authorization";
+
+}
