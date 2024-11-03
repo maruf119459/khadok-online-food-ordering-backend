6e9d355f130f788fb3240a397357bcfbc01bd402
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2024-11-03 16:29:18 +0600

    Create ContactInformation class

---
+++ src/main/java/com/khadok/model/ContactInformation.java
@@ -0,0 +1,12 @@
+package com.khadok.model;
+
+import lombok.Data;
+
+@Data
+
+public class ContactInformation {
+    private  String email;
+    private  String mobile;
+    private String twitter;
+    private String instagram;
+}
