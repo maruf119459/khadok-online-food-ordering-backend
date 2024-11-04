8ee7596b7c5b2883a1466920d4101cc53aa183a7
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2024-11-04 18:32:16 +0600

    Add /api/admin/ingredients, /api/admin/ingredients/category path

---
+++ src/main/java/com/khadok/controller/IngredientController.java
@@ -0,0 +1,38 @@
+package com.khadok.controller;
+
+import com.khadok.model.IngredientCategory;
+import com.khadok.model.IngredientsItem;
+import com.khadok.request.IngredientCategoryRequest;
+import com.khadok.request.IngredientRequest;
+import com.khadok.service.IngredientsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/ingredients")
+public class IngredientController {
+    @Autowired
+    private IngredientsService ingredientsService;
+
+    @PostMapping("/category")
+    public ResponseEntity<IngredientCategory> createIngredientCategory(
+            @RequestBody IngredientCategoryRequest req
+            ) throws Exception {
+        IngredientCategory item=ingredientsService.createIngredientCategory(req.getName(), req.getRestaurantId());
+        return new ResponseEntity<>(item, HttpStatus.CREATED);
+    }
+
+    @PostMapping()
+    public ResponseEntity<IngredientsItem> createIngredientItem(
+            @RequestBody IngredientRequest req
+    ) throws Exception {
+        IngredientsItem item=ingredientsService.crateIngredientItem(req.getRestaurantId(),req.getName(),req.getCategoryId());
+        return new ResponseEntity<>(item, HttpStatus.CREATED);
+    }
+
+   
+}
