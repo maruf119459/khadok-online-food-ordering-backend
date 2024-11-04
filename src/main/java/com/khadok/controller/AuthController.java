a73e13d311b0d949737ac1de53fed88694c991f0
maruf119459 <mdmohiuddinmaruf7@gmail.com>
2024-11-04 11:53:01 +0600

    Add signup path

---
+++ src/main/java/com/khadok/controller/AuthController.java
@@ -0,0 +1,82 @@
+package com.khadok.controller;
+
+import com.khadok.config.JwtProvider;
+import com.khadok.model.Cart;
+import com.khadok.model.USER_ROLE;
+import com.khadok.model.User;
+import com.khadok.repository.CartRepository;
+import com.khadok.repository.UserRepository;
+import com.khadok.request.LoginRequest;
+import com.khadok.response.AuthResponse;
+import com.khadok.service.CustomerUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @PostMapping("/signup")
+
+    public ResponseEntity<AuthResponse>createUserHandler(@RequestBody User user) throws Exception {
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+        if(isEmailExist != null){
+            throw new Exception("Email is already used with another account");
+        }
+
+        User createdUser=new User();
+        createdUser.setEmail(user.getEmail());
+        createdUser.setFullName(user.getFullName());
+        createdUser.setRole(user.getRole());
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepository.save(createdUser);
+
+        Cart cart = new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+   
+}
